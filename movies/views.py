867c9b2a0d5ed93f45f265873a0d8f292fb02809
Matthew Mateo <matthew.constante@uisel.edu.ec>
2026-05-19 10:16:08 -0500

    Examen Finalizado

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+def movie_index(request):
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'detail.html', {'movie': movie})
